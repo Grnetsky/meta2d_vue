af8c1896f46a1040846dfd711f32888765b184f0
Grnetsky <1101767893@qq.com>
2023-11-07 15:57:29 +0800

    add package-lock

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vuePlugin from '@vitejs/plugin-vue'
 import fs from "fs"
 import path from  "path"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
+  plugins: [vuePlugin(),
       devServerMiddleware()],
     resolve: {
         alias: {
